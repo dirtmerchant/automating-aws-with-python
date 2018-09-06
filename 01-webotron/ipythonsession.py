d3228ffc25c67f34bd6d79def847f2f6105bc2f8
Bert Bullough <bert@bertbullough.com>
2018-09-05 21:18:10 -0600

    setup project directory for webotron

---
+++ 01-webotron/ipythonsession.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import boto3
+session = boto3.Session(profile_name='pythonAutomation
+session = boto3.Session(profile_name='pythonAutomation)
+session = boto3.Session(profile_name='pythonAutomation')
+session
+s3 = session.resource('s3')
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+new_bucket = s3.create_bucket(Bucket='automatingawsbertbullough-boto3', CreateBucketConfiguration={'LocationConstraint': 'us-east-1'})
+new_bucket = s3.create_bucket(Bucket='automatingawsbertbullough-boto3', CreateBucketConfiguration={'LocationConstraint': 'us-east-1' })
+new_bucket = s3.create_bucket(Bucket='automatingawsbertbullough-boto3', CreateBucketConfiguration={'LocationConstraint': 'us-east-2' })
+new_bucket
+for bucket in s3.buckets.all():
+    
+    print(bucket)
+    
+for bucket in s3.buckets.all():
+    print(bucket)
+    
+ec2_client = session.client('ec2)
+ec2_client = session.client('ec2')
+ec2_client.all
+ec2_client.allocate
+ec2_client.allocate_hosts
+get_ipython().run_line_magic('history', '')
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 automating-aws-with-python
 
 v.1
+
+Webotron is a script that will sync a local directory to an s3 bucket
